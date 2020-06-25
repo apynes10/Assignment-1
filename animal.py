e8122f3515b8fa30e23469f6882328d65735b8ad
apynes10 <67440956+apynes10@users.noreply.github.com>
2020-06-25 14:38:50 -0700

    Add files via upload

---
+++ animal.py
@@ -0,0 +1,7 @@
+# Author: Alex Pynes
+# Date: 06/25/2020
+# Description: Asks the user for their favorite animal and then
+#              prints out that that is their favorite animal
+
+fave_animal = input("humpback whales")
+print("Your favorite animal is the humpback whale")
